07db4880f26b866caa78dae4300cea0b0d4c5a19
andreav <andreav.pub@gmail.com>
2022-06-04 15:17:00 +0200

    fix #18 - support for Redmine 5.x - Zeitwerk support

--- init.rb
+++ init.rb
@@ -9,20 +9,14 @@ Redmine::Plugin.register :redmine_landing_page do
   author_url 'https://github.com/biow0lf'
 end
 
-ActionDispatch::Callbacks.to_prepare do
-  require_dependency 'projects_controller'
-  ProjectsController.send(:include, RedmineLandingPage::Patches::ProjectsControllerPatch)
+require_dependency 'projects_controller'
+ProjectsController.send(:include, RedmineLandingPage::Patches::ProjectsControllerPatch)
 
-  require_dependency 'project'
-  Project.send(:include, RedmineLandingPage::Patches::ProjectPatch)
+require_dependency 'project'
+Project.send(:include, RedmineLandingPage::Patches::ProjectPatch)
 
-  require_dependency 'principal'
-  User.send(:include, RedmineLandingPage::Patches::UserPatch)
+require_dependency 'principal'
+User.send(:include, RedmineLandingPage::Patches::UserPatch)
 
-  require_dependency 'welcome_controller'
-  WelcomeController.send(:include, RedmineLandingPage::Patches::WelcomeControllerPatch)
-end
-
-require 'redmine_landing_page/hooks/view_projects_form_hook'
-require 'redmine_landing_page/hooks/view_my_account_hook'
-require 'redmine_landing_page/hooks/view_users_form_hook'
+require_dependency 'welcome_controller'
+WelcomeController.send(:include, RedmineLandingPage::Patches::WelcomeControllerPatch)
